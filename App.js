7accff8a3077e67117a0270934df4262e72a3685
pierreecft <91471897+pierreecft@users.noreply.github.com>
2023-02-23 11:54:27 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,49 +5,48 @@ import TikTok from './images/tikTok.png'
 import Linkedin from './images/LinkedIn.png'
 import girl from './images/girl.png'
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import About from './About.js';
 
 function App() {
-
-
-
   return (
-    <div className="App">
-      <header className="navbar">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className='textNavbar'>
-        <span className='about'> About us</span>
-        <span className='login'> Login</span>
-        <span className='signup'> Sign up</span>
-        </div>
-        
-      </header>
-      <div className='columns'>
-        <div className='column1'>
-          <div className='textpres'>
-              <h1 className='titlepres'>Size As </h1>
-              <h1 className='titlepres'>You Can Decide</h1>
-              <h3 className='descriptionpres'>SizeYouUp is a web platform that allows you to measure your dimensions in real time, and returns the ideal size according to your preferred brands.</h3>
+    <BrowserRouter>
+      <div className="App">
+        <header className="navbar">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className='textNavbar'>
+            <Link to="/about">
+              <span className='about'> About us</span>
+            </Link>
+            <span className='login'> Login</span>
+            <span className='signup'> Sign up</span>
           </div>
-          <button class="start-button">Let's start</button>
+        </header>
+        <div className='columns'>
+          <div className='column1'>
+            <div className='textpres'>
+                <h1 className='titlepres'>Size As </h1>
+                <h1 className='titlepres'>You Can Decide</h1>
+                <h3 className='descriptionpres'>SizeYouUp is a web platform that allows you to measure your dimensions in real time, and returns the ideal size according to your preferred brands.</h3>
+            </div>
+            <button class="start-button">Let's start</button>
+            <div>
+              <img src={insta} className="instalogo" alt="logo"/>
+              <img src={fb} className="fblogo" alt="logo"/>
+              <img src={TikTok} className="fblogo" alt="logo"/>
+              <img src={Linkedin} className="fblogo" alt="logo"/>
+            </div>
+          </div>
           <div>
-            <img src={insta} className="instalogo" alt="logo"/>
-            <img src={fb} className="fblogo" alt="logo"/>
-            <img src={TikTok} className="fblogo" alt="logo"/>
-            <img src={Linkedin} className="fblogo" alt="logo"/>
-          </div>
+          <img src={girl} className="girl" alt="logo"/>
         </div>
-        <div>
-        <img src={girl} className="girl" alt="logo"/>
+        </div>
       </div>
-      </div>
-      
-      
-      
-      
-      
-    </div>
+      <Route path="/about" component={About} />
+    </BrowserRouter>
   );
 }
 
 export default App;
+
